1051edf1b66a4943547ac9549dd23b62a41cb4d1
Kenji Tomita <kenjitomita@Kenjis-MacBook-Pro.local>
2025-06-04 12:58:32 -0700

    Remove conflicting app folder

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Explicitly disable the App Router
+  experimental: {
+    appDir: false
+  }
+}
+
+module.exports = nextConfig 
